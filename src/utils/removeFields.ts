33271dcd82e1f98bf937f0f3ec0d3225c0c86119
Santosl2 <mfilype2019@gmail.com>
2026-06-25 16:13:35 -0300

    fix: update return type of removeUnusedFields function to object

--- src/utils/removeFields.ts
+++ src/utils/removeFields.ts
@@ -1,8 +1,10 @@
-export function removeUnusedFields(obj: object): void {
+export function removeUnusedFields(obj: object): object {
   
   Object.keys(obj).forEach((key) => {
+    //@ts-expect-error
     const value = obj[key];
     if (key.startsWith("$$")) {
+      //@ts-expect-error
       delete obj[key];
     } else if (typeof value === "object" && value !== null) {
       removeUnusedFields(value);
